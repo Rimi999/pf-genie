d8aa69f10e4243b4ab99c60c82bde3e2f7121bda
Rimi999 <all_wom@naver.com>
2021-06-25 13:34:09 +0900

    navi 완성

--- js/index.js
+++ js/index.js
@@ -1,11 +1,24 @@
 /*************** 글로벌 설정 *****************/
-
+$('.sub-wrap').slideUp(0)
 
 /*************** 사용자 함수 *****************/
 
 
 /*************** 이벤트 등록 *****************/
+$('.navi-wrapper .navi').on('click', onNaviClick)
+$('.navi-wrapper .navi').on('mouseover', onNaviHover)
+$('.navi-wrapper').on('mouseleave', onNaviLeave)
 
 
 /*************** 이벤트 콜백 *****************/
+function onNaviClick() {
+	$('.sub-wrap').stop().slidToggle(300)
+}
 
+function onNaviHover() {
+	$('.sub-wrap').stop().slideDown(300)
+}
+
+function onNaviLeave() {
+	$('.sub-wrap').stop().slideUp(300)
+}
