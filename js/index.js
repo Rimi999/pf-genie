8ebdfea739560b3e8db090b167397f0f3712fc6c
Rimi999 <all_wom@naver.com>
2021-06-26 17:47:26 +0900

    json 문제 해결

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ init()
 function init() {
 	navi()
 	times()
+	chart()
 }
 
 
@@ -19,19 +20,36 @@ function navi() {
 	function onNaviClick() {
 		$('.sub-wrapper').stop().slidToggle(300)
 	}
-	
+
 	function onNaviHover() {
 		$('.sub-wrapper').stop().slideDown(300)
 	}
-	
+
 	function onNaviLeave() {
 		$('.sub-wrapper').stop().slideUp(300)
 	}
 }
 
-
 function times() {
 	var $chartTimes = $('.chart-wrapper .times')
 	$chartTimes.find('.date').text(moment().format('YYYY. M. D.'))
 	$chartTimes.find('.time').text(moment().format('hh:mm'))
+}
+
+function chart() {
+	var $chartWrap = $('.chart-wrapper .chart-wrap');
+
+	function onGetData(r) {
+		r.chart.forEach(function (v, i) {
+			var html = '';
+			html += '<li class="chart">'
+			html += '<div class="rank">'+ v.id +'</div>'
+			html += '<div><img src="'+ v.src +'"></div>'
+			html += '<div class="sing">'+ v.sing +'</div>'
+			html += '<div class="singer">'+ v.singer +'</div>'
+			html += '</li>'
+			$chartWrap.append(html);
+		})
+	}
+	$.get('../json/chart.json', onGetData);
 }
