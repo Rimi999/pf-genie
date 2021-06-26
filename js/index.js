3f5f011f03ab1eae99c5f73c816695abe0c8e7c5
Rimi999 <all_wom@naver.com>
2021-06-26 11:29:17 +0900

    chart json 작업중

--- js/index.js
+++ js/index.js
@@ -1,25 +1,31 @@
 /*************** 글로벌 설정 *****************/
 
+init()
 
 /*************** 사용자 함수 *****************/
-$('.sub-wrapper').slideUp(0)
-
-
-/*************** 이벤트 등록 *****************/
-$('.navi-wrapper .navi').on('click', onNaviClick)
-$('.navi-wrapper .navi').on('mouseover', onNaviHover)
-$('.navi-wrapper').on('mouseleave', onNaviLeave)
+function init() {
+	navi() 
+}
 
 
 /*************** 이벤트 콜백 *****************/
-function onNaviClick() {
-	$('.sub-wrapper').stop().slidToggle(300)
+function navi() {
+
+	$('.navi-wrapper .navi').on('click', onNaviClick)
+	$('.navi-wrapper .navi').on('mouseover', onNaviHover)
+	$('.navi-wrapper').on('mouseleave', onNaviLeave)
+
+	function onNaviClick() {
+		$('.sub-wrapper').stop().slidToggle(300)
+	}
+	
+	function onNaviHover() {
+		$('.sub-wrapper').stop().slideDown(300)
+	}
+	
+	function onNaviLeave() {
+		$('.sub-wrapper').stop().slideUp(300)
+	}
 }
 
-function onNaviHover() {
-	$('.sub-wrapper').stop().slideDown(300)
-}
 
-function onNaviLeave() {
-	$('.sub-wrapper').stop().slideUp(300)
-}
